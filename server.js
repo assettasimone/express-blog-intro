bbfa880417c7aa4db41a28834bdee19357d403a4
assettasimone <assettasimone@gmail.com>
2025-09-04 17:20:55 +0200

    add: include blog articles data structure

--- server.js
+++ server.js
@@ -6,6 +6,39 @@ app.get('/', (req, res) => {
     res.send('Server del mio Blog')
 })
 
+const blogArticles = [
+    {
+        title: "il mio primo Articolo",
+        content: "Questo è il contenuto del mio primo articolo"
+        coverImage: "imgs/anatra.jpg"
+        tags: ["tag1", "tag2", "tag3"]
+    },
+    {
+        title: "il mio secondo Articolo",
+        content: "Questo è il contenuto del mio primo articolo"
+        coverImage: "imgs/anatra.jpg"
+        tags: ["tag1", "tag2", "tag3"]
+    },
+    {
+        title: "il mio terzo Articolo",
+        content: "Questo è il contenuto del mio primo articolo"
+        coverImage: "imgs/anatra.jpg"
+        tags: ["tag1", "tag2", "tag3"]
+    },
+    {
+        title: "il mio quarto Articolo",
+        content: "Questo è il contenuto del mio primo articolo"
+        coverImage: "imgs/anatra.jpg"
+        tags: ["tag1", "tag2", "tag3"]
+    },
+    {
+        title: "il mio quinto Articolo",
+        content: "Questo è il contenuto del mio primo articolo"
+        coverImage: "imgs/anatra.jpg"
+        tags: ["tag1", "tag2", "tag3"]
+    }
+]
+
 app.listen(port, () => {
     console.log(`App listening on port http://localhost:${port}`);
 })
