5d7ed8ad3c28e541ff105e63cc579b10fdcf1399
assettasimone <assettasimone@gmail.com>
2025-09-04 17:21:50 +0200

    fix: correct syntax errors in blog ar

--- server.js
+++ server.js
@@ -15,29 +15,31 @@ const blogArticles = [
     },
     {
         title: "il mio secondo Articolo",
-        content: "Questo è il contenuto del mio primo articolo"
-        coverImage: "imgs/anatra.jpg"
+        content: "Questo è il contenuto del mio secondo articolo"
+        coverImage: "imgs/anatra2.jpg"
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "il mio terzo Articolo",
-        content: "Questo è il contenuto del mio primo articolo"
-        coverImage: "imgs/anatra.jpg"
+        content: "Questo è il contenuto del mio terzo articolo"
+        coverImage: "imgs/anatra3.jpg"
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "il mio quarto Articolo",
-        content: "Questo è il contenuto del mio primo articolo"
-        coverImage: "imgs/anatra.jpg"
+        content: "Questo è il contenuto del mio quarto articolo"
+        coverImage: "imgs/anatra4.jpg"
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "il mio quinto Articolo",
-        content: "Questo è il contenuto del mio primo articolo"
-        coverImage: "imgs/anatra.jpg"
+        content: "Questo è il contenuto del mio quinto articolo"
+        coverImage: "imgs/anatra5.jpg"
         tags: ["tag1", "tag2", "tag3"]
     }
 ]
+
+
 
 app.listen(port, () => {
     console.log(`App listening on port http://localhost:${port}`);
